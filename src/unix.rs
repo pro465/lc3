a8648dd2fb77449bf8c01b810898ef8ab2988466
Proloy Mishra <67726964+pro465@users.noreply.github.com>
2022-04-25 10:33:49 +0530

    remove `unix::check_key`

--- src/unix.rs
+++ src/unix.rs
@@ -2,21 +2,6 @@ use std::mem::MaybeUninit as MU;
 
 static mut orig_tio: MU<libc::termios> = MU::uninit();
 
-pub fn check_key() -> u16 {
-    unsafe {
-        let mut readfds = MU::uninit();
-        libc::FD_ZERO(readfds.as_mut_ptr());
-        libc::FD_SET(libc::STDIN_FILENO, readfds.as_mut_ptr());
-
-        let mut timeout = libc::timeval {
-            tv_sec: 0,
-            tv_usec: 0,
-        };
-
-        (libc::select(1, readfds.as_mut_ptr(), 0 as _, 0 as _, &mut timeout) != 0) as u16
-    }
-}
-
 pub fn disable_input_buffering() {
     unsafe {
         libc::tcgetattr(libc::STDIN_FILENO, orig_tio.as_mut_ptr());
